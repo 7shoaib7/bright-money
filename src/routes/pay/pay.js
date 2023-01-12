49c5c34d92183925910c40244429cfe68b9d2d96
7shoaib7 <69447663+7shoaib7@users.noreply.github.com>
2023-01-12 18:12:29 +0530

    highlighted-bill-topay

--- src/routes/pay/pay.js
+++ src/routes/pay/pay.js
@@ -17,18 +17,20 @@ const Pay = () => {
   let count = 0;
   let total = 0
   for (let i = 0; i < billSorted.length; i++) {
-    if (total <= monthlyBudget) {
+    if (total < monthlyBudget) {
       total += parseInt(billSorted[i].amount);
       billSorted[i].paid = true;
       count++;
     }
     else {
+      total -= parseInt(billSorted[i-1].amount);
       break;
     }
   }
 
   const handleEdit = () => {
     setEdit(true)
+    setEditBudgetValue(monthlyBudget)
   }
 
   const handleSave = () => {
@@ -64,7 +66,7 @@ const Pay = () => {
         </thead>
         <tbody>
           {billSorted.map((bill, index) => (
-            <tr key={bill.id} className={index < count ? "highlight" : ""}>
+            <tr key={bill.id} className={index+1 < count ? "highlight" : ""}>
               <td>{bill.description}</td>
               <td>{bill.category}</td>
               <td>₹ {bill.amount}</td>
@@ -76,7 +78,7 @@ const Pay = () => {
       <div className="total-amount">
         <strong>Total Amount to be paid:</strong> ₹{total}
         {bills.length - count !== 0 ?
-          <p className="remaining-amount">(Add extra amount to your monthly budget to pay remaining {bills.length - count} bills)</p>
+          <p className="remaining-amount">(Add extra amount to your monthly budget to pay remaining {bills.length+1 - count} bills)</p>
           : null}
       </div>
     </>
